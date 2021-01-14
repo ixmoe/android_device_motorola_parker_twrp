3fca9ab3890a55415c6fb553d485feb17ef0714c
Beta Soft <betaxab@gmail.com>
2021-01-14 19:01:33 +0800

    parker: add sensors_class.ko

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -7,6 +7,7 @@ load()
     if [ ! -z "$is_fastboot_twrp" ]; then
         insmod /vendor/lib/modules/aw8695.ko
         insmod /vendor/lib/modules/goodix_fod_mmi.ko
+        insmod /vendor/lib/modules/sensors_class.ko
     else
         mkdir /v
         suffix=$(getprop ro.boot.slot_suffix)
@@ -19,9 +20,11 @@ load()
         mkdir -p /vendor/lib/modules
         cp /v/lib/modules/aw8695.ko /vendor/lib/modules/aw8695.ko
         cp /v/lib/modules/goodix_fod_mmi.ko /vendor/lib/modules/goodix_fod_mmi.ko
+        cp /v/lib/modules/sensors_class.ko /vendor/lib/modules/sensors_class.ko
 
         insmod /vendor/lib/modules/aw8695.ko
         insmod /vendor/lib/modules/goodix_fod_mmi.ko
+        insmod /vendor/lib/modules/sensors_class.ko
         umount /v
         rmdir /v
     fi
